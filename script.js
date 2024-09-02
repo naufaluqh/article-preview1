f849dff66ac715f2abe0c92e6dd44aef44786728
naufaluqh <naufaluqh@gmail.com>
2024-09-02 17:11:24 +0700

    finish

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
+// Memilih elemen yang dibutuhkan
 const shareContainer = document.querySelector(".share-container");
-const shareButton = shareContainer.querySelector(".btn-share");
+const shareButtons = document.querySelectorAll(".btn-share");
 const shareOptions = shareContainer.querySelector(".share-options");
+const footer = document.querySelector(".footer");
+const footerToggle = document.querySelector(".footer-toogle");
 
-shareButton.addEventListener("click", () => {
+// Fungsi untuk menampilkan opsi share di tampilan desktop
+function toggleShareOptionsDesktop() {
   shareOptions.style.display =
     shareOptions.style.display === "flex" ? "none" : "flex";
-  shareButton.classList.toggle("active");
+}
+
+// Fungsi untuk menukar tampilan footer di tampilan mobile
+function toggleFooterMobile() {
+  if (footer.style.display !== "none") {
+    footer.style.display = "none";
+    footerToggle.style.display = "flex";
+  } else {
+    footer.style.display = "flex";
+    footerToggle.style.display = "none";
+  }
+}
+
+// Menambahkan event listener pada setiap tombol share
+shareButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.stopPropagation(); // Mencegah bubbling event
+
+    if (window.innerWidth > 767) {
+      toggleShareOptionsDesktop();
+    } else {
+      toggleFooterMobile();
+    }
+  });
 });
 
-// Menutup opsi share ketika mengklik di luar
+// Menutup opsi share ketika klik di luar kontainer di tampilan desktop
 document.addEventListener("click", (event) => {
-  if (!shareContainer.contains(event.target)) {
+  if (!shareContainer.contains(event.target) && window.innerWidth > 767) {
     shareOptions.style.display = "none";
-    shareButton.classList.remove("active");
   }
 });
